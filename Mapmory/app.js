a1d0f8165272132012db6485b36e1b91c9295d0e
Hong <fheldgktpdy@naver.com>
2021-05-09 15:14:26 +0900

    create every url
    every url create, gitignore txt

--- Mapmory/app.js
+++ Mapmory/app.js
@@ -4,13 +4,53 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-//Home
-app.get('/', (req,res) => res.send('Hello world!'));
 
+app.use(session({ 
+    secret: 'keyboard cat', 
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+ }));
+
+//Home
+app.get('/', function(req,res){
+    res.send("Home page");
+})
 //login
+app.put('/login',function(req,res){
+    res.send("Login page");
+})
 //logout
+app.put('/logout',function(req,res){
+    res.send("Logout page");
+})
 //register
+app.post('/register',function(req,res){
+    res.send('register page');
+})
 //Mapmain
+app.get('/Mapmain',function(req,res){
+    res.send('Mapmain page');
+})
+//Read post
+app.get('/Mapmain/posts/:postId',function(req,res){
+    var postId = req.params.postId;
+    res.send("Mapmain post page", postId);
+})
+//Create post
+app.post('/Mapmain/posts',function(req,res){
+    res.send("Mapmain create page");
+})
+//Update post
+app.put('/Mapmain/posts/:postId',function(req,res){
+    var postId = req.params.postId;
+    res.send("Mapmain update page");
+})
+//Delete post
+app.delete('/Mapmain/posts/:postId',function(req,res){
+    var postId = req.params.postId;
+    res.send("Mapmain update page");
+})
 //Mapmain/category/:_category
 //Mapmain/post/:_pk
 
