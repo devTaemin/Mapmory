93ba1b4d84aede350c5cdbedf5f99117231ea78a
Hong <fheldgktpdy@naver.com>
2021-05-09 14:32:32 +0900

    Update app.js
    body-parser add, url 카테고리 주석정리

--- Mapmory/app.js
+++ Mapmory/app.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+//Home
+app.get('/', (req,res) => res.send('Hello world!'));
+
+//login
+//logout
+//register
+//Mapmain
+//Mapmain/category/:_category
+//Mapmain/post/:_pk
+
+app.listen(3000, ()=> console.log('Example app listening on port 3000!'));
