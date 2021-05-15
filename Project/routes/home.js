2adcdb35cce35705fcab8c8761b819341b7019d0
Taemin Im <hugeman@khu.ac.kr>
2021-05-15 17:17:35 +0900

    Update Project directory
    - 포스팅 구현
    - 회원가입, 로그인, 로그아웃 구현

--- Project/routes/home.js
+++ Project/routes/home.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+require('../config/passport');
 
 router.get('/', function(req,res){
     res.render('home/welcome');
@@ -8,5 +10,54 @@ router.get('/', function(req,res){
 router.get('/about', function(req,res){
     res.render('home/about');
 });
+
+//Login
+router.get('/login', function(req,res){
+    var username = req.flash('username')[0];
+    var errors = req.flash('errors')[0] || {};
+    res.render('home/login',{
+        username:username,
+        errors:errors
+    });
+});
+
+
+//Post login
+router.post('/login',
+    function(req,res,next){
+        var errors = {};
+        var isValid = true;
+
+        if(!req.body.username){
+            isValid = false;
+            errors.username = 'Username is required!';
+        }
+        if(!req.body.password){
+            isValid = false;
+            errors.password = 'Password is required!';
+        }
+
+        if(isValid){
+            next();
+        }
+        else{
+            req.flash('errors',errors);
+            res.redirect('/login');
+        }
+    },
+    passport.authenticate('local-login', {
+        successRedirect : '/posts',
+        failureRedirect : '/login'
+    }
+));
+
+
+// Logout
+router.get('/logout', function(req,res){
+    req.logout();
+    res.redirect('/');
+});
+
+
 
 module.exports = router;
--- Project/routes/posts.js
+++ Project/routes/posts.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const Post = require('../models/Post');
+var express = require('express');
+var router = express.Router();
+var Post = require('../models/Post');
 
 
 // Post home
