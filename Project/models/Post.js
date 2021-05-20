2a07a51a848f67260642ea961b58ef1037ed7d02
Hong <fheldgktpdy@naver.com>
2021-05-20 15:21:54 +0900

    Update project directory
    - new,show 기능에 map추가.(marker도)
    - mongoose에 address 추가해서 marker 불러오게끔 구현(디자인은 구림)
    - edit 기능 미구현. edit 구현전 저장

--- Project/models/Post.js
+++ Project/models/Post.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // Declare the schemea of post
 var postSchema = mongoose.Schema({
     title:{type:String, required:[true, 'Title is required!']},
+    address:{type:String, required:[true, 'address is required!']},
     body:{type:String, required:[true, 'Content is required!']},
     author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
     createdAt:{type:Date, default:Date.now},
